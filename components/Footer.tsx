48409d4ce502d239a1adae7e4a884382c763551a
Hiếu Huỳnh <mike.beplus@gmail.com>
2025-01-08 22:50:56 +0700

    Create brand master

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,9 +18,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="/">
-            Tailwind Nextjs Theme
-          </Link>
+          <Link href="/">Tailwind Nextjs Theme</Link>
         </div>
       </div>
     </footer>
